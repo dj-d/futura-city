7f0d0de854b7500bde54cb917f334a21ce290e24
Daniele Albanese <daniele.albanese98@gmail.com>
2023-08-30 19:09:32 +0200

    feat/fix (app): added deploy region 'eu-north-1', added EnergyEfficiencyApiGatewayStack, fixed Stacks import path and reformat code

--- app.py
+++ app.py
@@ -1,15 +1,52 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
-from data_analytics.data_analytics_stack import DataAnalyticsStack
-from energy_efficiency.energy_efficiency_stack import EnergyEfficiencyStack
-from smart_traffic.smart_traffic_stack import SmartTrafficStack
+from stacks.data_analytics.data_analytics_stack import DataAnalyticsStack
+from stacks.energy_efficiency.energy_efficiency_stack import EnergyEfficiencyStack
+from stacks.smart_traffic.smart_traffic_stack import SmartTrafficStack
+from stacks.api_gateway.api_gateway_stack import ApiGatewayStack, ApiGatewayModel
 
+
+env_EU = cdk.Environment(region='eu-north-1')
 
 app = cdk.App()
 
-data_analytics_stack = DataAnalyticsStack(app, "DataAnalyticsStack")
-energy_efficiency_stack = EnergyEfficiencyStack(app, "EnergyEfficiencyStack")
-smart_traffic_stack = SmartTrafficStack(app, "SmartTrafficStack")
+data_analytics_stack = DataAnalyticsStack(
+    scope=app,
+    construct_id='DataAnalyticsStack',
+    env=env_EU
+)
+
+energy_efficiency_stack = EnergyEfficiencyStack(
+    scope=app,
+    construct_id='EnergyEfficiencyStack',
+    env=env_EU
+)
+
+smart_traffic_stack = SmartTrafficStack(
+    scope=app,
+    construct_id='SmartTrafficStack',
+    env=env_EU
+)
+
+energy_efficiency_api_gw_stack = ApiGatewayStack(
+    scope=app,
+    construct_id='EnergyEfficiencyApiGateway',
+    env=env_EU,
+    api_name='EnergyEfficiencyApi',
+    api_id='energy-efficiency-api',
+    endpoint='energy-efficiency',
+    allowed_methods=['GET', 'POST'],
+    api_models=[
+        ApiGatewayModel(
+            method='GET',
+            lambda_integration=energy_efficiency_stack.lambda_rd
+        ),
+        ApiGatewayModel(
+            method='POST',
+            lambda_integration=energy_efficiency_stack.lambda_wr
+        )
+    ]
+)
 
 app.synth()
