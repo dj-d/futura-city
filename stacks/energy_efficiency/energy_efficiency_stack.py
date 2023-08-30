668b8a1c1a39a45693dae5ff7ac13493017ff7fb
Daniele Albanese <daniele.albanese98@gmail.com>
2023-08-30 19:00:24 +0200

    feat (energy_efficiency): init vpc, lambda functions and RDS

--- stacks/energy_efficiency/energy_efficiency_stack.py
+++ stacks/energy_efficiency/energy_efficiency_stack.py
@@ -1,10 +1,81 @@
 from constructs import Construct
 from aws_cdk import (
-    Stack
+    Stack,
+    aws_ec2 as ec2,
+    aws_lambda as lambda_,
+    aws_rds as rds
 )
+
 
 class EnergyEfficiencyStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        self.subnet_name = 'DB'
+
+        self.__create_vpc()
+
+        self.lambda_wr = self.__create_lambda(
+            name='LambdaWrite',
+            dest_vpc=self.vpc,
+            # dest_subnet=self.db_subnet,
+            code_path='stacks/energy_efficiency/lambda_write'
+            )
+
+        self.lambda_rd = self.__create_lambda(
+            name='LambdaRead',
+            dest_vpc=self.vpc,
+            code_path='stacks/energy_efficiency/lambda_read'
+            )
+
+        self.__create_db()
+
+    def __create_vpc(self) -> None:
+        vpc_construct_id = 'energy-efficiency-vpc'
+        vpc_name = 'EnergyEfficiencyVpc'
+        vpc_cidr = ec2.IpAddresses.cidr('10.0.0.0/24')
+
+        self.db_subnet = ec2.SubnetConfiguration(
+            name=self.subnet_name, 
+            subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS, 
+            cidr_mask=28
+        )
+
+        self.vpc: ec2.Vpc = ec2.Vpc(
+            self,
+            vpc_construct_id,
+            vpc_name=vpc_name,
+            # max_azs=1,
+            ip_addresses=vpc_cidr,
+            subnet_configuration=[
+                self.db_subnet
+            ],
+            nat_gateways=0
+        )
+
+    def __create_lambda(self, name: str, code_path: str, dest_vpc: ec2.Vpc, dest_subnet=None) -> lambda_.Function:
+        base_lambda = lambda_.Function(
+            self,
+            name,
+            handler='lambda-handler.handler',  # Name_of_file.Name_of_function_to_call
+            runtime=lambda_.Runtime.PYTHON_3_7,
+            vpc=dest_vpc,
+            vpc_subnets=dest_subnet,
+            code=lambda_.Code.from_asset(code_path)  # Path_to_folder_containing_lambda_handler.py
+        )
+
+        return base_lambda
+
+    def __create_db(self) -> None:
+        db_construct_id = 'energy-efficiency-db'
+        db_name = 'EnergyEfficiencyDb'
+
+        self.db = rds.DatabaseInstance(
+            self,
+            db_construct_id,
+            database_name=db_name,
+            engine=rds.DatabaseInstanceEngine.mysql(
+                version=rds.MysqlEngineVersion.VER_5_7
+            ),
+            vpc=self.vpc
+        )
