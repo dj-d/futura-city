796c6f8000b98f947042ebb61bbc1ffee20c721f
Daniele Albanese <daniele.albanese98@gmail.com>
2023-09-12 00:15:27 +0200

    feat (api_gateway): added 'References', added param 'description' to '__create_rest_api' and convert to private those variables that do not need to leave the class

--- stacks/api_gateway/api_gateway_stack.py
+++ stacks/api_gateway/api_gateway_stack.py
@@ -1,3 +1,9 @@
+"""
+References:
+  - AWS Doc:
+      - aws_apigateway.RestApi: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_apigateway/RestApi.html#aws_cdk.aws_apigateway.RestApi
+"""
+
 from dataclasses import dataclass
 
 from constructs import Construct
@@ -15,19 +21,31 @@ class ApiGatewayModel:
 
 
 class ApiGatewayStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, api_name: str, api_id: str, endpoint: str, allowed_methods: list, api_models: list[ApiGatewayModel], **kwargs):
+    def __init__(
+            self,
+            scope: Construct,
+            construct_id: str,
+            apigw_description: str,
+            api_id: str,
+            api_name: str,
+            endpoint: str,
+            allowed_methods: list,
+            api_models: list[ApiGatewayModel],
+            **kwargs
+    ):
         super().__init__(scope, construct_id, **kwargs)
 
         self.__endpoint = endpoint
         self.__allowed_methods = allowed_methods
-        self.api_models = api_models
+        self.__api_models = api_models
 
-        self.api_gateway = self.__create_rest_api(
+        self.__api_gateway = self.__create_rest_api(
             api_name=api_name,
-            api_id=api_id
+            api_id=api_id,
+            description=apigw_description
         )
 
-        self.entity = self.api_gateway.root.add_resource(
+        self.__entity = self.__api_gateway.root.add_resource(
             self.__endpoint,
             default_cors_preflight_options=apigw_.CorsOptions(
                 allow_origins=apigw_.Cors.ALL_ORIGINS,
@@ -35,8 +53,8 @@ class ApiGatewayStack(Stack):
             )
         )
 
-        for api_model in self.api_models:
-            self.entity.add_method(
+        for api_model in self.__api_models:
+            self.__entity.add_method(
                 api_model.method,
                 self._lambda_integration(api_model.lambda_integration),
                 method_responses=[
@@ -49,10 +67,11 @@ class ApiGatewayStack(Stack):
                 ]
             )
 
-    def __create_rest_api(self, api_name: str, api_id: str) -> apigw_.RestApi:
+    def __create_rest_api(self, api_id: str, description: str, api_name: str) -> apigw_.RestApi:
         return apigw_.RestApi(
             self,
-            api_id,
+            id=api_id,
+            description=description,
             rest_api_name=api_name
         )
     
@@ -70,4 +89,3 @@ class ApiGatewayStack(Stack):
                 )
             ]
         )
-    
