45e70a439d1c5505d5fda9ddc75f782ef63875cb
Daniele Albanese <daniele.albanese98@gmail.com>
2023-09-19 16:45:00 +0200

    feat (smart_traffic/lambda_init): added lambda function code to create table 'smart_traffic'

---
+++ stacks/smart_traffic/lambda_init/lambda-handler.py
@@ -0,0 +1,125 @@
+"""
+References:
+    - Handle Lambda errors in API Gateway: https://docs.aws.amazon.com/apigateway/latest/developerguide/handle-errors-in-lambda-integration.html
+"""
+
+# TODO: Add logger
+
+import os
+import boto3
+import pymysql
+from typing import Union, Optional
+
+
+def handler(event, context):  # TODO: Add doc
+    secret = get_secret()
+
+    # Connect to database
+    try:
+        conn = pymysql.connect(
+            host=secret["host"],
+            port=secret["port"],
+            user=secret["username"],
+            password=secret["password"],
+            database=secret["dbname"]
+        )
+
+        print('Connected to database')
+    except pymysql.MySQLError as e:
+        # print(f'Error: {e}')
+        # raise e
+
+        return make_response(
+            status_code=500,
+            error=e
+        )
+
+    # Insert data
+    try:
+        with conn.cursor() as cur:
+            if event['action'] == 'create':
+                sql = '''
+                        CREATE TABLE IF NOT EXISTS smart_traffic (
+                            id INT PRIMARY KEY AUTO_INCREMENT,
+                            name VARCHAR(255)
+                        )
+                    '''
+                action_message = 'Created table'
+            elif event['action'] == 'delete':
+                sql = 'DROP TABLE IF EXISTS smart_traffic'
+                action_message = 'Deleted table'
+            else:
+                return make_response(
+                    status_code=400,
+                    body='Invalid action'
+                )
+
+            cur.execute(sql)
+            conn.commit()
+
+            # print(action_message)
+    except pymysql.MySQLError as e:
+        # print(f'Error: {e}')
+        # raise e
+
+        return make_response(
+            status_code=500,
+            error=e
+        )
+
+    # Close connection
+    try:
+        conn.close()
+        # print('Closed connection')
+    except pymysql.MySQLError as e:
+        # print(f'Error: {e}')
+        # raise e
+
+        return make_response(
+            status_code=500,
+            error=e
+        )
+
+    return make_response(
+        status_code=200,
+        body=action_message
+    )
+
+
+def get_secret():  # TODO: Add doc
+    # Get environment variables
+    db_secret_arn = os.environ['DB_SECRET_ARN']
+
+    # Get secret from Secrets Manager
+    client = boto3.client('secretsmanager')
+    get_secret_value_response = client.get_secret_value(
+        SecretId=db_secret_arn
+    )
+    secret = eval(get_secret_value_response['SecretString'])
+
+    return secret
+
+
+def make_response(status_code: int, body: Union[dict, list, str] = None, error: Optional[pymysql.MySQLError] = None) -> dict:  # TODO: Add doc
+    if error is not None:
+        print(f'etype: {type(error)}')
+        code, message = error.args
+        return {
+            'isBase64Encoded': False,
+            'statusCode': status_code,
+            'body': message,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+
+    return {
+        'isBase64Encoded': False,
+        'statusCode': status_code,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': body
+    }
