564b6e46ddccf00da3171eb28713a8e359e25a30
Daniele Albanese <daniele.albanese98@gmail.com>
2023-09-19 16:58:22 +0200

    feat (data_analytics): updated creation of vpc with my custom lib and updated references

--- stacks/data_analytics/data_analytics_stack.py
+++ stacks/data_analytics/data_analytics_stack.py
@@ -1,19 +1,28 @@
 """
 References:
-    - AWS Doc:
-        - aws_ec2.Vpc: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/Vpc.html#aws_cdk.aws_ec2.Vpc
-        - aws_ec2.SubnetConfiguration: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/SubnetConfiguration.html
-        - aws_ec2.SubnetType: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/SubnetType.html
-        - aws_ec2.IpAddresses: ttps://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/IpAddresses.html#aws_cdk.aws_ec2.IpAddresses
+  - AWS Doc:
+      - aws_ec2.SubnetType: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/SubnetType.html
+      - aws_ec2.InterfaceVpcEndpointAwsService: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/InterfaceVpcEndpointAwsService.html#interfacevpcendpointawsservice
+      - aws_ec2.SubnetSelection: https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/SubnetSelection.html#subnetselection
 
-    - Examples:
-        - aws-cdk-rfcs: https://github.com/aws/aws-cdk-rfcs/blob/main/text/0340-firehose-l2.md
+  - Examples:
+      - aws-cdk-rfcs: https://github.com/aws/aws-cdk-rfcs/blob/main/text/0340-firehose-l2.md
 """
 
 from constructs import Construct
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2
+)
+
+from lib.dataclasses import (
+    ServicePrefix,
+    VpcConfig,
+    SubnetConfig
+)
+
+from lib.services import (
+    create_vpc
 )
 
 
@@ -26,32 +35,23 @@ class DataAnalyticsStack(Stack):
             **kwargs
         )
 
-        self.service_id_prefix = 'da-'
-        self.service_name_prefix = 'DA'
-
-        self.__create_vpc()
-
-    def __create_vpc(self) -> None:  # TODO: Add doc
-        vpc_construct_id = self.service_id_prefix + 'vpc'
-        vpc_name = self.service_name_prefix + 'Vpc'
-        vpc_cidr = ec2.IpAddresses.cidr('10.0.0.0/24')
-
-        private_subnet_id = self.service_id_prefix + 'private-subnet'
-        self.__private_subnet_type = ec2.SubnetType.PRIVATE_ISOLATED
-
-        private_subnet = ec2.SubnetConfiguration(
-            name=private_subnet_id,
-            subnet_type=self.__private_subnet_type,
-            cidr_mask=28
+        self.service_prefix = ServicePrefix(
+            id='da-',
+            name='Da'
         )
 
-        self.__vpc = ec2.Vpc(
-            self,
-            vpc_construct_id,
-            vpc_name=vpc_name,
-            max_azs=2,
-            ip_addresses=vpc_cidr,
-            subnet_configuration=[
-                private_subnet
-            ]
+        # ---------------------------------------- #
+        # VPC
+        # ---------------------------------------- #
+        self.__vpc, self.__private_subnet_type = create_vpc(
+            instance_class=self,
+            service_prefix=self.service_prefix,
+            vpc_config=VpcConfig(
+                cidr='10.0.0.0/24'
+            ),
+            subnet_config=SubnetConfig(
+                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                subnet_id='private-subnet',
+                cidr_mask=28
+            )
         )
